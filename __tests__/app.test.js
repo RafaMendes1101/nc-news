1e0d30f91c796de82fb880efbd547db830effb7c
Rafael <rafael.mendes1101@gmail.com>
2021-11-02 07:57:35 +0000

    added GET /api/topics routes and testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
-const { seed } = require("../db/seeds/seed.js");
+const seed = require("../db/seeds/seed.js");
 const request = require("supertest");
 const app = require("../app.js");
 
@@ -9,13 +9,32 @@ afterAll(() => db.end());
 
 describe("nc-news app", () => {
   describe("testing testData object", () => {
-    test("test testData object", () => {
+    it("test testData object", () => {
       expect(testData).toMatchObject({
         articleData: expect.any(Array),
         commentData: expect.any(Array),
         topicData: expect.any(Array),
         userData: expect.any(Array),
       });
+    });
+  });
+  describe("GET /api/topics", () => {
+    test("status 200 responds with an array of topics", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.topics).toBeInstanceOf(Array);
+          expect(body.topics).toHaveLength(3);
+          body.topics.forEach((topic) => {
+            expect(topic).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        });
     });
   });
 });
